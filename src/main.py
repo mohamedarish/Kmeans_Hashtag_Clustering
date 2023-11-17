027b9d8192b8d73f6d7c8a8845606e451b5b865f
mohamed arish <arish1248@gmail.com>
2023-11-17 19:16:46 +0530

    Test for google chat

--- src/main.py
+++ src/main.py
@@ -28,6 +28,7 @@ my_auth = requests_oauthlib.OAuth1(
 
 
 # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
 # auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 #
 # api = tweepy.API(auth, wait_on_rate_limit=True)
