3004c6341aab101a333cb18b57850d19f43eaab7
mohamed arish <arish1248@gmail.com>
2023-11-23 17:13:26 +0530

    Added tfidf but for some reason, it can't parse the csv file ℑ<^

--- src/main.py
+++ src/main.py
@@ -1,28 +1,23 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode, split
 
-# Create a Spark session
 spark = SparkSession.builder.appName("WordOccurrence").getOrCreate()
 
-# Read the CSV file into a Spark DataFrame
 csv_file_path = "assets/init_data.csv"
 df = spark.read.option("header", "false").csv(csv_file_path)
 
-# Extract the words from the 4th column using the split function
+print(df.count())
+
 words_df = df.withColumn("words", split(col("_c4"), " "))
 
-# Explode the array of words into separate rows
 exploded_df = words_df.select(
     col("_c0").alias("ID"), explode(col("words")).alias("word")
 )
 
-# Group by the word and count its occurrences
 word_occurrences = exploded_df.groupBy("word").count().orderBy("count")
 
-# Show the results
 word_occurrences.show(word_occurrences.count(), truncate=False)
 
 print(word_occurrences.count())
 
-# Stop the Spark session
 spark.stop()
