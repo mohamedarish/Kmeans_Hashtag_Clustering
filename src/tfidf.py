d8a9005764d85a95559c8f16292fad6a251702e7
mohamed arish <arish1248@gmail.com>
2023-11-23 17:13:58 +0530

    Removed comments

--- src/tfidf.py
+++ src/tfidf.py
@@ -1,24 +1,17 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Read the CSV file into a Pandas DataFrame
 csv_file_path = "assets/init_data.csv"
 df = pd.read_csv(csv_file_path)
 
-# Assuming the text data is in the 5th column, change the column index accordingly
 text_data = df.iloc[:, 4]
 
-# Create a TfidfVectorizer
 vectorizer = TfidfVectorizer()
 
-# Fit and transform the text data
 tfidf_matrix = vectorizer.fit_transform(text_data)
 
-# Get the feature names (words) from the vectorizer
 feature_names = vectorizer.get_feature_names_out()
 
-# Create a DataFrame with TF-IDF values for each word
 tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=feature_names)
 
-# Display the TF-IDF DataFrame
 print(tfidf_df)
